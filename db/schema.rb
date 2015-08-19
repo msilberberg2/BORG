1ebeed118bc3072d685ec7dd581073b9ac6a069e
Martin Silberberg <msilberg@Martins-MacBook-Air.local>
2015-08-19 10:40:11 -0400

    Prep work for forum functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819213804) do
+ActiveRecord::Schema.define(version: 20150818213811) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
